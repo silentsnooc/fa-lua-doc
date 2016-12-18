625d08848fe2b0446fb2932119c345bea6ea33b2
Stefan Falk <falk.stefan@gmx.at>
2016-12-18 16:59:25 +0100

    removing old make cmd script

--- make.cmd
+++
@@ -1,8 +0,0 @@
-rem ldoc -a -s ldoc\ -d E:\git\faforever\fa-lua-doc\fa ..\fa\lua\
-
-cd ../fa/lua
-
-@echo off
-for /d %%D in (*) do ldoc -a -s ..\..\fa-lua-doc\ldoc -d ..\..\fa-lua-doc\lua\%%D %%~fD
-
-cd ../../fa-lua-doc
